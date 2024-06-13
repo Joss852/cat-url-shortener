d217e5f32cea6820491eac57cc83f02de20c39ee
Josseph Morales <jjosseph.morales@gmail.com>
2024-06-12 18:17:23 -0600

    update footer and input components

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,10 @@
 export default function Footer() {
   return (
-    <footer className='mt-8 text-sm text-[#FF8C00]'>&copy; 2023 Catly. All rights reserved.</footer>
+    <footer className='mt-8 text-sm text-[#FF8C00]'>
+      Made with 🧡 by{' '}
+      <a target='_blank' referrerPolicy='no-referrer' href='https://github.com/Joss852' className='underline'>
+        @Joss852
+      </a>
+    </footer>
   )
 }
--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,7 +1,12 @@
-export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  reference: React.RefObject<HTMLInputElement>
+}
+
+export default function Input(props: InputProps) {
   return (
     <input
       {...props}
+      ref={props.reference}
       className={`class="flex h-10 rounded-md border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${props.className}`}
     />
   )
